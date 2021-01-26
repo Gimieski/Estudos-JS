7a33b104f40b39f9c935a1f16460f07d52513d94
Gabriel Gimieski <77447947+Gimieski@users.noreply.github.com>
2021-01-26 15:46:53 -0300

    Novos exercicios

---
+++ Projetos/Js.js
@@ -0,0 +1,21 @@
+function escrever(){
+var msg= document.querySelector('.mensagem')
+var foto=document.querySelector(".img");
+var data = new Date();
+var hora=data.getHours();
+// aqui é a hora do nosso computador, podemos odificar pegando hora=13 por ex.
+msg.innerHTML="Agora são "+hora+"horas";
+
+if(hora >=0 && hora <=12){
+// se hora for maior que zero e tambem menor que 12
+foto.src='imagens/manha.jpg'
+document.body.style.backgroundColor='#e2cd9f'
+}else if(hora >=12 && hora<=18){
+    foto.src='imagens/tarde.jpg'
+    document.body.style.backgroundColor='#b9846f'
+}else{
+    foto.src='imagens/noite.jpg';
+    document.body.style.backgroundColor='#515154'
+}
+
+}
